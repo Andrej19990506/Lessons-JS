f0ed132f272cf1173473823f260c61a1e0b3cc32
Andrej19990506 <91632348+Andrej19990506@users.noreply.github.com>
2022-06-02 00:14:48 +0700

    ferstlesson

--- LessonsJS.js
+++ LessonsJS.js
@@ -1,2 +1,21 @@
 //Lesson 0ne 01.06.2022
-alert("hello, World")
+class  CalcTable {
+    constructor(parentSelector){
+        this.parentElement=document.querySelector(parentSelector);
+    }
+
+    createTable(row, column){
+        this.parentElement.innerHTML="";
+        this.table=document.createElement('table');
+        this.parentElement.appendChild(this.table);
+    }
+}
+let table= new CalcTable('div');
+table.createTable();
+
+let table2 = new CalcTable('#div2')
+
+table2.createTable()
+
+
+
