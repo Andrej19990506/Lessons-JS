7ba07bf7b350a799d2b724940b136178f9e6a662
Andrej19990506 <91632348+Andrej19990506@users.noreply.github.com>
2022-06-15 23:09:32 +0700

    lesson...

--- LessonsJS.js
+++ LessonsJS.js
@@ -1,21 +1,142 @@
 //Lesson 0ne 01.06.2022
-class  CalcTable {
+class CalcTable {
     constructor(parentSelector){
+        this.parentSelector = parentSelector;
         this.parentElement=document.querySelector(parentSelector);
     }
-
     createTable(row, column){
+        row = Number(row);
+        column = Number(column);
         this.parentElement.innerHTML="";
         this.table=document.createElement('table');
         this.parentElement.appendChild(this.table);
+        this.table.classList.add('table');
+        this.row = row;
+        this.column = column;
+        for( let i = 0; i<row; i++){
+            let tr = document.createElement('tr');
+            tr.classList.add('table__row');
+            this.table.appendChild(tr);
+            for( let j = 0; j<column; j++){
+                let td = document.createElement('td');
+                td.classList.add('table__column');
+                tr.appendChild(td);
+                td.innerHTML="";
+                td.contentEditable = "true"
+            }
+            //let lasttd = document.createElement('td')
+            //lasttd.classList.add('last-td')
+            //tr.appendChild(lasttd)
+            //lasttd.innerHTML="+"
+        }
+        
+        //let tr = document.createElement('tr');
+        //tr.classList.add('table__row');
+        //this.table.appendChild(tr);
+        //for(let i = 0; i<column+1; i++){
+            //let lasttd = document.createElement('td')
+            //lasttd.classList.add('last-td')
+            //tr.appendChild(lasttd)
+            //lasttd.innerHTML="+"
+        //}
+        
+
+        
+        this.table.onclick = this.clickcel.bind(this);
+        //this.table.onclick = this.newtd.bind(this);
+    }
+    
+    
+
+    
+               //Двойной клик
+        //let td = event.target.closest('td'); // (1)
+        //if (!td) return; // (2)
+        //if (!this.table.contains(td)) return;
+        //let inputclick = document.createElement('textarea');
+        //td.appendChild(inputclick);
+    clickcel(event){    
+        //console.log(this);
+        //this.createTable(this.row+1, this.column+1)
+        let td = event.target.closest('td'); // (1)
+        if (!td) return; // (2)
+        if (!this.table.contains(td)) return; // (3)
+        if(this.currenCell){
+                this.currenCell.classList.remove("border-td");
+            }
+            this.currenCell = td;
+            this.currenCell.classList.add("border-td");
+        //if(){
+            
+        //}
     }
 }
-let table= new CalcTable('div');
-table.createTable();
+class TableBuild{
+    constructor(parentSelector, table){
+        this.table = table;
+        this.parentSelector = parentSelector;
+        this.parentElement=document.querySelector(parentSelector);
+        }
+    tableBuild(){
+        this.buildtable = document.createElement('div');
+        this.addTable = document.createElement('div');
+        this.addTable.innerHTML="+";
+        this.addTable.classList.add('add-table');
+        this.deliteTable = document.createElement('div');
+        this.deliteTable.innerHTML="-";
+        this.deliteTable.classList.add('delite-table');
+        this.parentElement.appendChild(this.buildtable);
+        this.buildtable.appendChild(this.addTable);
+        this.buildtable.appendChild(this.deliteTable);
 
-let table2 = new CalcTable('#div2')
+        this.buildtable.onclick = this.inputtext.bind(this);
+    }
 
-table2.createTable()
+    inputtext(event){  
+        let td = event.target.closest('.add-table'); // (1)
+        if (!td) return; // (2)
+        if (!this.buildtable.contains(td)) return; // (3)
+        if(this.currenCell){
+                this.table.createTable(this.row+1, this.column+1);
+            }
+            this.currenCell = td;
+            //console.log(this);
+        //this.table=new CalcTable(this.parentSelector);
+    }
+} 
+    //let table = new CalcTable();
+    //let buildtble = new TableBuild(table)
+
+
+    
+class TableCreater{
+    constructor(parentSelector){
+        this.parentSelector = parentSelector;
+        this.parentElement=document.querySelector(parentSelector);
+    }
+
+    createForm(){
+        this.parentElement.innerHTML = "";
+        this.inputRow = document.createElement('input');
+        this.inputColumn = document.createElement('input');
+        this.buttonForm = document.createElement('button');
+        this.buttonForm.innerHTML ='Button create'
+        this.parentElement.appendChild(this.inputRow);
+        this.parentElement.appendChild(this.inputColumn);
+        this.parentElement.appendChild(this.buttonForm);
+        this.buttonForm.onclick=this.clickButton.bind(this);
+    }
+
+    clickButton(){
+        this.table=new CalcTable(this.parentSelector);
+        this.table.createTable(this.inputRow.value , this.inputColumn.value );
+        this.buildtable = new TableBuild(this.parentSelector);
+        this.buildtable.tableBuild()
+
+    }
+}
+let tableCreater = new TableCreater('div');
+tableCreater.createForm()
 
 
 
